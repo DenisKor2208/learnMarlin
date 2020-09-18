3b805070ad178d4c66cf8170ae3c440791e03cc7
DenisKor2208 <dkorotin@list.ru>
2020-09-18 20:11:41 +0700

    результат четвертого задания

--- task_4.php
+++ task_4.php
@@ -34,66 +34,58 @@
                         <div class="panel-content">
                             <?php
                             $param = [
-                            [   "title" => "Главная",
-                                "bg" => "",
-                                "width" => "",
-                                "aria-valuenow" => "",
-                                "aria-valuemin" => "",
-                                "aria-valuemax" => ""
+                            [   "title" => "My Tasks",
+                                "value" => "130 / 500",
+                                "bg" => "bg-fusion-400",
+                                "width" => "65%",
+                                "aria-valuenow" => "65",
+                                "aria-valuemin" => "0",
+                                "aria-valuemax" => "100"
                             ],
-                            [   "title" => "PHP",
-                                "bg" => "",
-                                "width" => "",
-                                "aria-valuenow" => "",
-                                "aria-valuemin" => "",
-                                "aria-valuemax" => ""
+                            [   "title" => "Transfered",
+                                "value" => "440 TB",
+                                "bg" => "bg-success-500",
+                                "width" => "34%",
+                                "aria-valuenow" => "34",
+                                "aria-valuemin" => "0",
+                                "aria-valuemax" => "100"
                             ],
-                            [   "title" => "Функции",
-                                "bg" => "",
-                                "width" => "",
-                                "aria-valuenow" => "",
-                                "aria-valuemin" => "",
-                                "aria-valuemax" => ""
+                            [   "title" => "Bugs Squashed",
+                                "value" => "77%",
+                                "bg" => "bg-info-400",
+                                "width" => "77%",
+                                "aria-valuenow" => "77",
+                                "aria-valuemin" => "0",
+                                "aria-valuemax" => "100"
                             ],
-                            [   "title" => "Функции",
-                                "bg" => "",
-                                "width" => "",
-                                "aria-valuenow" => "",
-                                "aria-valuemin" => "",
-                                "aria-valuemax" => ""
+                            [   "title" => "User Testing",
+                                "value" => "7 days",
+                                "bg" => "bg-primary-300",
+                                "width" => "84%",
+                                "aria-valuenow" => "84",
+                                "aria-valuemin" => "0",
+                                "aria-valuemax" => "100"
                             ]
                             ];
                             ?>
+                            <?php foreach ($param as $value): ?>
+
                             <div class="d-flex mt-2">
-                                My Tasks
-                                <span class="d-inline-block ml-auto">130 / 500</span>
+                                <?php echo $value['title'];?>
+                                <span class="d-inline-block ml-auto"><?php echo $value['value'];?></span>
                             </div>
 
                             <div class="progress progress-sm mb-3">
-                                <div class="progress-bar bg-fusion-400" role="progressbar" style="width: 65%;" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                                <div class="progress-bar <?php echo $value['bg'];?>"
+                                            role="progressbar"
+                                            style="width: <?php echo $value['width'];?>;"
+                                            aria-valuenow="<?php echo $value['aria-valuenow'];?>"
+                                            aria-valuemin="<?php echo $value['aria-valuemin'];?>"
+                                            aria-valuemax="<?php echo $value['aria-valuemax'];?>">
+                                </div>
                             </div>
 
-                            <div class="d-flex">
-                                Transfered
-                                <span class="d-inline-block ml-auto">440 TB</span>
-                            </div>
-                            <div class="progress progress-sm mb-3">
-                                <div class="progress-bar bg-success-500" role="progressbar" style="width: 34%;" aria-valuenow="34" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="d-flex">
-                                Bugs Squashed
-                                <span class="d-inline-block ml-auto">77%</span>
-                            </div>
-                            <div class="progress progress-sm mb-3">
-                                <div class="progress-bar bg-info-400" role="progressbar" style="width: 77%;" aria-valuenow="77" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="d-flex">
-                                User Testing
-                                <span class="d-inline-block ml-auto">7 days</span>
-                            </div>
-                            <div class="progress progress-sm mb-g">
-                                <div class="progress-bar bg-primary-300" role="progressbar" style="width: 84%;" aria-valuenow="84" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
+                            <?php endforeach; ?>
                         </div>
                     </div>
                 </div>
